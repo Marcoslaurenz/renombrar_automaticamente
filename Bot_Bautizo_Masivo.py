d15f45463acf39f27d0806ba583150f92126f076
Marcoslaurenz <75302939+Marcoslaurenz@users.noreply.github.com>
2021-04-19 10:39:58 +0800

    Update Bot_Bautizo_Masivo.py

--- Bot_Bautizo_Masivo.py
+++ Bot_Bautizo_Masivo.py
@@ -3,13 +3,13 @@ print("************************************")
 print("*Renombrar archivos automaticamente*")
 print("************************************ \n")
 nombre = input("Ingrese el nombre desea asignar: ")
-
+tipo_de_documento = input("Ingresa el tipo de documento: ")
 ubicacion = input("Ingresa la ubicacion de los archivos: ") 
 os.chdir(ubicacion)
 i=1
 for file in os.listdir():
      src=file
-     dst=nombre+str(i)+".jpg"
+     dst=nombre+str(i)+"."+tipo_de_decumento
      os.rename(src,dst)
      i+=1
 print("Tus archivos han sido renombrados exitosamente.")
